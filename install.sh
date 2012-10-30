9e60e2fa49fae24637df52f19b715d25361f0943
Detegr <detegr@gmail.com>
2012-10-30 09:04:44 +0200

    Added submodule initialization + bundle installing to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 mv $HOME/.vimrc $HOME/.vimrc_bak
 mv $HOME/.vim $HOME/.vim_bak
+git submodule init
+git submodule update
 cp -r .vim/ $HOME/.vim
 mv $HOME/.vim/vimrc $HOME/.vimrc
+vim +BundleInstall +qall
