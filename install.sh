a3a9f8193e4949e9b22f4cfdae24d1b3317803ae
Detegr <detegr@gmail.com>
2013-01-27 14:19:03 +0200

    Fixes

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ if [[ -d "$HOME/.vim" ]]; then
 	echo "Backing up $HOME/.vim to $HOME/.vim_bak"
 	if [[ -d "$HOME/.vim_bak" ]]; then
 		echo "Removing old backup"
-		rm -r $HOME/.vim_bak
+		rm -rf $HOME/.vim_bak
 	fi
 	mv $HOME/.vim $HOME/.vim_bak
 fi
 git submodule init
 git submodule update
 echo "Copying .vim to $HOME/.vim"
-cp -r .vim/ $HOME/.vim
+cp -rf .vim/ $HOME/.vim
 echo "Copying new .vimrc to $HOME/.vimrc"
 mv $HOME/.vim/vimrc $HOME/.vimrc
 echo "Installing bundles"
